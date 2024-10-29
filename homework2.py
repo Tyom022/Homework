f8e996d89819de4dcad8888ee127d9944b6a4d23
Tyom022 <78855656+Tyom022@users.noreply.github.com>
2024-10-29 23:28:43 +0400

    Changes

--- homework2.py
+++ homework2.py
@@ -4,3 +4,19 @@ if var_X == 0:
     print("head".capitalize())
 else:
     print("tail".capitalize())
+    
+# TASK 2
+var_list = []
+
+for i in range(0,5):
+    var_list.append(random.randint(1,30))
+print(var_list, "Final list")
+
+# TASK 3
+
+string_list = ["Manuk", "Sargis", "Hask", "Artyom"]
+
+sorted_list = sorted(string_list)
+print(sorted_list)
+res = sorted_list[1:-1]
+print(res)
